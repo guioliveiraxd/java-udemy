cdae4feb6b7d7c3111a9671a4b1d661409f875a6
Guilherme <guioliveira1177@gmail.com>
2026-08-22 17:36:10 -0300

    dia2 curso udemy de java

--- Dia2/projeto/src/Main.java
+++ Dia2/projeto/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
                 1789, 2035, 1899, 1456, 2013,
                 1458, 2458, 1254, 1472, 2365,
                 1456, 2165, 1457, 2456};
+
         String[] my_array2 = {
                 "Java",
                 "Python",
@@ -14,9 +15,18 @@ public class Main {
                 "C Programming",
                 "C++"
         };
+
         System.out.println(Arrays.toString(my_array1));
+
+        // 1. Ordena e imprime o array numérico (ordem crescente)
+        Arrays.sort(my_array1);
+        System.out.println("Array numérico ordenado: " + Arrays.toString(my_array1));
 
 
         System.out.println(Arrays.toString(my_array2));
+
+        // 2. Ordena e imprime o array de Strings (ordem alfabética)
+        Arrays.sort(my_array2);
+        System.out.println("Array de texto ordenado: " + Arrays.toString(my_array2));
     }
 }
