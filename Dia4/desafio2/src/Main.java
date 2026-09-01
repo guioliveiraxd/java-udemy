8552c0a139d3fa90f2a9fbd7de5b6c642b523634
Guilherme <guioliveira1177@gmail.com>
2026-09-01 08:17:55 -0300

    dia4 - desafios

---
+++ Dia4/desafio2/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String spaces = "Hello world";
+
+        for(int i = 0; i <= spaces.length()-1; i++)
+            System.out.print(spaces.charAt(i) + " ");
+
+    }
+}
---
+++ Dia4/parte2/src/Main.java
@@ -0,0 +1,30 @@
+//public class Main {
+//    public static void main(String[] args) {
+//        String[] cars = {"BMW", "Tesla", "Fiat", "Ferrari"};
+//
+//        for (int i = 0; i < cars.length; i++)
+//            System.out.println(cars[i]);
+//
+//
+//    }
+//}
+//public class Main{
+//    public static void main(String[] args) {
+//        for (int i = 0; i < 1; i++)
+//            for( int j = 0; j < 3; j++)
+//                System.out.println(i + " " + j);
+//
+//
+//    }
+//
+//}
+public class Main {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println(i + " - Hi");
+            i++;
+
+        }
+    }
+}
